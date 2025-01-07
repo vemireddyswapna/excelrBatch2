97a90370c23c3f98108f408e8bec318ff7193814
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-07 10:12:18 +0530

    Add files via upload

---
+++ Demo015.java
@@ -0,0 +1,23 @@
+public class Demo015 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        System.out.println("Unary Plus (+x): " + (+x));
+
+        
+        System.out.println("Unary Minus (-x): " + (-x));
+        System.out.println("Pre-increment (++x): " + (++x)); 
+
+        System.out.println("Post-increment (x++): " + (x++)); 
+        System.out.println("Value of x after post-increment: " + x);
+
+        
+        System.out.println("Pre-decrement (--y): " + (--y)); 
+
+        
+        System.out.println("Post-decrement (y--): " + (y--)); 
+        System.out.println("Value of y after post-decrement: " + y); 
+
+    }
+}
