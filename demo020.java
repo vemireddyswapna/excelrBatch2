396f175550f7911ace740e99703d133488160372
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-09 11:51:13 +0530

    Add files via upload

---
+++ demo020.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class demo020 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FizzBizz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Bizz");
+        } else {
+            System.out.println("The number is not a multiple of 3 or 5.");
+        }
+
+        scanner.close();
+    }
+}
