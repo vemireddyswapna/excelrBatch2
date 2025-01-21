104f3be7aab1635dfe0edfbe10eca4c30e1ecb83
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-21 10:07:20 +0530

    Add files via upload

---
+++ Demo032.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class Demo032{
+    public static void main(String[] args) {
+        // Create a HashSet to store strings
+        HashSet<String> stringSet = new HashSet<>();
+
+        // Add some strings to the HashSet
+        stringSet.add("Apple");
+        stringSet.add("Banana");
+        stringSet.add("Cherry");
+
+        // Print the HashSet
+        System.out.println(stringSet);
+    }
+}
+
