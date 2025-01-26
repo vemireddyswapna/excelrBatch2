bc3654cd5554ee6f3501bd4390344b93cfa03f5a
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-26 10:56:05 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo3 {
+
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 20;
+        num3 = 30;
+        if ((num1 < num2) && (num2 < num3)) {
+            System.out.println(x:"numl is greater ");
+        }
+        else if ((num2 > num1) && (num2 > num3)) {
+            System.out.println(x:"num2 is greater ");
+        }
+        else ((num3 > num1) && (num3 > num2)) {
+            System.out.println(x:"num3 is greater");
+        }
+    }
+}
