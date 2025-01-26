ccb2674e167076242eb16119d1f4f9a0c4b2908c
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-26 10:52:07 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+public class Demo2 {
+
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 10;
+        num2 = 20;
+        if (num1 < num2){
+            System.out.println(x:"numl is greater ");
+        }
+        else if (num2 > num1) {
+            System.out.println(x:"num2 is greater ");
+        }
+}
