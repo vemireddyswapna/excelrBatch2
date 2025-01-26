c3fd7161ad0f996aef2b6946a25ff2ddbab34343
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-26 10:56:22 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,9 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        // Loop through numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
