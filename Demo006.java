ed72b156911880d760ad79ce1bf02832d2d31c98
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-26 11:04:23 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String username, password;
+        username = "sindhu";
+        password = "sindhu@123";
+        while((username == "sindhu") && (password == "sindhu@123")) {
+            System.out.println("HAPPY NEW YEAR");
+            break;
+        }
+    }
+}
