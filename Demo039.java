4136a877a99ab90455ae56986babcec3702f2735
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-01-27 09:34:31 +0530

    Add files via upload

---
+++ Demo039.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Demo039 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println(ch + " is an uppercase letter.");
+        } else if (ch >= 'a' && ch <= 'z') {
+            System.out.println(ch + " is a lowercase letter.");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println(ch + " is a digit.");
+        } else {
+            System.out.println(ch + " is not an alphabetic character or digit.");
+            scanner.close();
+ }
+}
+}
