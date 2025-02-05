385a3eb5457f9f83864aaa2742501dba03ab693a
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-02-05 10:02:02 +0530

    Add files via upload

---
+++ Demo062.java
@@ -0,0 +1,8 @@
+public class Demo062 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        System.out.println("1st Element: " + array[0]);
+        System.out.println("2nd Element: " + array[1]);
+        System.out.println("Last Element: " + array[array.length - 1]); 
+    }
+}
