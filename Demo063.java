449c13894409e632720a500ee27a5eef5174b40e
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-02-06 10:01:23 +0530

    Add files via upload

---
+++ Demo063.java
@@ -0,0 +1,11 @@
+public class Demo063 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+
+        System.out.println("1st Element: " + array[0]);
+        System.out.println("2nd Element: " + array[1]);
+        System.out.println("Last Element: " + array[array.length - 1]);
+        int sum = array[0] + array[1];
+        System.out.println("Sum of 1st and 2nd elements: " + sum);
+    }
+}
