a7a57d23b7b9891b0fdacad4a61ee6398bc9a966
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-02-06 09:58:51 +0530

    Delete Demo066.java

--- Demo066.java
+++
@@ -1,9 +0,0 @@
-public class Demo066 {
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50}; 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("Index Value: " + arr[i]);
-        }
-    }
-}
-
