f6e20271c368e2f76f02c37ec0431575d3f6c2a3
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-02-06 09:51:10 +0530

    Update Demo063.java

--- Demo063.java
+++ Demo063.java
@@ -5,8 +5,6 @@ public class Demo063 {
         System.out.println("1st Element: " + array[0]);
         System.out.println("2nd Element: " + array[1]);
         System.out.println("Last Element: " + array[array.length - 1]);
-
-        // Fixing the sum calculation
         int sum = array[0] + array[1];
         System.out.println("Sum of 1st and 2nd elements: " + sum);
     }
