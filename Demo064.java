4f1a3b94ea59bdef88e73512bcbb9aac10c5156d
vemireddyswapna <2311cs010684@mallareddyuniversity.ac.in>
2025-02-06 10:01:46 +0530

    Add files via upload

---
+++ Demo064.java
@@ -0,0 +1,16 @@
+public class Demo064 {
+    public void method(){
+        int[] arr = {10, 20, 30, 40, 50};
+        int temp = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = temp;
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void main(String[] args) {
+      Demo064 d1=new Demo064();
+      d1.method();  
+    }}
+
